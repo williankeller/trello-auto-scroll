b160fdc61f90500a8c5aa384a55a25fe23d2bbaa
Diego Ocko Martins <dmart166@its.jnj.com>
2017-09-21 16:36:26 -0300

    Transform Into A Chrome Extension

---
+++ scripts/script.js
@@ -0,0 +1,54 @@
+
+var executeScroll = function() {
+  // Get board variables.
+  var $cards = $('.list-wrapper').not('.js-add-list'),
+    cards = 0,
+    scrollTime = 1500,
+    animation = 800,
+    margin = 10;
+
+  $cards.each(function () {
+    cards += $(this).width();
+  });
+
+  var $board = $('#board'),
+    scrollStep = (cards / $cards.length),
+    scrollPrev = margin,
+    // Initial step is the same of all of the other steps.
+    leftPos = 0;
+
+  setInterval(function () {
+    // While cards is more bigger then current scroll.
+    if ((cards >= leftPos) && ($board.scrollLeft() != scrollPrev)) {
+      // Save previus scroll.
+      scrollPrev = $board.scrollLeft();
+
+      // Increment scroll.
+      leftPos += parseInt(scrollStep + margin);
+
+      // Animate to right.
+      $board.animate({
+        scrollLeft: leftPos
+      }, animation);
+    }
+    else {
+      // Animate to left.
+      $board.animate({
+        scrollLeft: 0
+      }, animation);
+
+      // Clear sroll.
+      leftPos = 0;
+      scrollPrev = margin;
+    }
+  }, scrollTime);
+};
+
+$('document').ready(function() {
+  intervalListener = setInterval(function () {
+    if ($('#board').length > 0) {
+      executeScroll();
+      window.clearInterval(intervalListener);
+    }
+  }, 200);
+});
