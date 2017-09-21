72e11436d086d881fdf5348bc946ec9b8def2ae6
Willian Keller <will_levinski@hotmail.com>
2017-09-21 20:55:10 -0300

    Improve script file

--- scripts/script.js
+++ scripts/script.js
@@ -1,54 +1,69 @@
+(function ($) {
+  "use strict";
 
-var executeScroll = function() {
-  // Get board variables.
-  var $cards = $('.list-wrapper').not('.js-add-list'),
-    cards = 0,
-    scrollTime = 3500,
-    animation = 800,
-    margin = 10;
+  /**
+   * Scroll board to last card and back to the first one.
+   *
+   * @returns {Boolean}
+   */
+  var trelloAutoScroll = function () {
+    // Get board variables.
+    var $board = $('#board'),
+      $cards = $('.list-wrapper').not('.js-add-list'),
+      cards = 0,
+      scrollTime = 3500,
+      animation = 800,
+      margin = 10;
 
-  $cards.each(function () {
-    cards += $(this).width();
-  });
+    // Map all cards and get there width size.
+    $cards.each(function () {
+      cards += $(this).width();
+    });
 
-  var $board = $('#board'),
-    scrollStep = (cards / $cards.length),
-    scrollPrev = margin,
     // Initial step is the same of all of the other steps.
-    leftPos = 0;
-
-  setInterval(function () {
-    // While cards is more bigger then current scroll.
-    if ((cards >= leftPos) && ($board.scrollLeft() != scrollPrev)) {
-      // Save previus scroll.
-      scrollPrev = $board.scrollLeft();
-
-      // Increment scroll.
-      leftPos += parseInt(scrollStep + margin);
-
-      // Animate to right.
-      $board.animate({
-        scrollLeft: leftPos
-      }, animation);
-    }
-    else {
-      // Animate to left.
-      $board.animate({
-        scrollLeft: 0
-      }, animation);
-
-      // Clear sroll.
-      leftPos = 0;
+    var leftPos = 0,
+      scrollStep = (cards / $cards.length),
       scrollPrev = margin;
-    }
-  }, scrollTime);
-};
 
-$('document').ready(function() {
+    // Set interval as a loop to stop by each card.
+    setInterval(function () {
+      // While cards is more bigger then current scroll.
+      if ((cards >= leftPos) && ($board.scrollLeft() != scrollPrev)) {
+        // Save previus scroll.
+        scrollPrev = $board.scrollLeft();
+
+        // Increment scroll.
+        leftPos += parseInt(scrollStep + margin);
+
+        // Animate to right.
+        $board.animate({
+          scrollLeft: leftPos
+        }, animation);
+      } else {
+        // Animate to left.
+        $board.animate({
+          scrollLeft: 0
+        }, animation);
+
+        // Clear sroll.
+        leftPos = 0;
+        scrollPrev = margin;
+      }
+    }, scrollTime);
+  };
+
+  /**
+   * Interval to detect if Trello board is already loaded.
+   */
   intervalListener = setInterval(function () {
+    // Board loaded already?
     if ($('#board').length > 0) {
-      executeScroll();
+      // Load function.
+      trelloAutoScroll();
+
+      // Stop interval.
       window.clearInterval(intervalListener);
     }
-  }, 200);
-});
+  }, 100);
+
+})(jQuery);
