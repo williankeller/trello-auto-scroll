ee2c44a70244713434454acba6681630cbf9a248
Willian Keller <will_levinski@hotmail.com>
2017-09-29 21:54:09 -0300

    Implement option to Scroll back in steps

--- scripts/options.js
+++ scripts/options.js
@@ -1,5 +1,5 @@
 // Set a status directly at the status div.
-var set_status = function (text, classname) {
+var setStatus = function (text, classname) {
   var status = document.getElementById('status');
   status.textContent = text;
   status.className += classname;
@@ -13,17 +13,19 @@ var set_status = function (text, classname) {
 // Saves options to chrome.storage.sync.
 var optionsSave = function () {
   var scrollTimeValue = document.getElementById('scroll-time').value;
+  var scrollStepBackValue = document.getElementById('scroll-step-back').checked;
   var animationTimeValue = document.getElementById('animation-time').value;
 
   if ((! scrollTimeValue) || (! animationTimeValue)) {
-    set_status('Please fill all items in the form', 'error');
+    setStatus('Please fill all items in the form', 'error');
     return false;
   }
 
   chrome.storage.sync.set({
     scrollTime: scrollTimeValue,
+    scrollStepBack: scrollStepBackValue,
     animationTime: animationTimeValue
-  }, set_status('Options Saved!', 'success'));
+  }, setStatus('Options Saved!', 'success'));
 };
 
 // Restores select box and checkbox state using the preferences
@@ -32,9 +34,11 @@ var optionsRestore = function () {
   // Use default value color = 'red' and likesColor = true.
   chrome.storage.sync.get({
     scrollTime: 3500,
+    scrollStepBack: false,
     animationTime: 800
   }, function (items) {
     document.getElementById('scroll-time').value = items.scrollTime;
+    document.getElementById('scroll-step-back').checked = items.scrollStepBack;
     document.getElementById('animation-time').value = items.animationTime;
   });
 };
