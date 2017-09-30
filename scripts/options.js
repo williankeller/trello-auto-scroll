c964e48db68484a27577bb29d476b39ed454ff94
Willian Keller <will_levinski@hotmail.com>
2017-09-29 22:21:03 -0300

    Implement action to Scroll back in steps

--- scripts/options.js
+++ scripts/options.js
@@ -1,47 +1,51 @@
-// Set a status directly at the status div.
-var setStatus = function (text, classname) {
-  var status = document.getElementById('status');
-  status.textContent = text;
-  status.className += classname;
+(function (chrome, document) {
+  'use strict';
 
-  setTimeout(function () {
-    status.textContent = '';
-    status.className = '';
-  }, 1500);
-};
+  // Set a status directly at the status div.
+  var setStatus = function (text, classname) {
+    var status = document.getElementById('status');
+    status.textContent = text;
+    status.className += classname;
 
-// Saves options to chrome.storage.sync.
-var optionsSave = function () {
-  var scrollTimeValue = document.getElementById('scroll-time').value;
-  var scrollStepBackValue = document.getElementById('scroll-step-back').checked;
-  var animationTimeValue = document.getElementById('animation-time').value;
+    setTimeout(function () {
+      status.textContent = '';
+      status.className = '';
+    }, 1500);
+  };
 
-  if ((! scrollTimeValue) || (! animationTimeValue)) {
-    setStatus('Please fill all items in the form', 'error');
-    return false;
-  }
+  // Saves options to chrome.storage.sync.
+  var optionsSave = function () {
+    var scrollTimeValue = document.getElementById('scroll-time').value;
+    var scrollStepBackValue = document.getElementById('scroll-step-back').checked;
+    var animationTimeValue = document.getElementById('animation-time').value;
 
-  chrome.storage.sync.set({
-    scrollTime: scrollTimeValue,
-    scrollStepBack: scrollStepBackValue,
-    animationTime: animationTimeValue
-  }, setStatus('Options Saved!', 'success'));
-};
+    if ((! scrollTimeValue) || (! animationTimeValue)) {
+      setStatus('Please fill all items in the form', 'error');
+      return false;
+    }
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-var optionsRestore = function () {
-  // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get({
-    scrollTime: 3500,
-    scrollStepBack: false,
-    animationTime: 800
-  }, function (items) {
-    document.getElementById('scroll-time').value = items.scrollTime;
-    document.getElementById('scroll-step-back').checked = items.scrollStepBack;
-    document.getElementById('animation-time').value = items.animationTime;
-  });
-};
+    chrome.storage.sync.set({
+      scrollTime: scrollTimeValue,
+      scrollStepBack: scrollStepBackValue,
+      animationTime: animationTimeValue
+    }, setStatus('Options Saved!', 'success'));
+  };
 
-document.addEventListener('DOMContentLoaded', optionsRestore);
-document.getElementById('save').addEventListener('click', optionsSave);
+  // Restores select box and checkbox state using the preferences
+  // Stored in chrome.storage.
+  var optionsRestore = function () {
+    // Use default value color = 'red' and likesColor = true.
+    chrome.storage.sync.get({
+      scrollTime: 3500,
+      scrollStepBack: false,
+      animationTime: 800
+    }, function (items) {
+      document.getElementById('scroll-time').value = items.scrollTime;
+      document.getElementById('scroll-step-back').checked = items.scrollStepBack;
+      document.getElementById('animation-time').value = items.animationTime;
+    });
+  };
+
+  document.addEventListener('DOMContentLoaded', optionsRestore);
+  document.getElementById('save').addEventListener('click', optionsSave);
+})(chrome, document);
