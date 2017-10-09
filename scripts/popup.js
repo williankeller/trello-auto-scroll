d9240fe9a4b7f9ff0a7397ce8171d7b951c57112
Willian Keller <will_levinski@hotmail.com>
2017-10-08 21:27:49 -0300

    Improving start/stop buttons #2

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,5 +1,13 @@
-(function ($, chrome, document) {
+(function (j, chrome, document) {
   'use strict';
+  j('.options-scroll').on('click', function () {
+    if (chrome.runtime.openOptionsPage) {
+      // New way to open options pages, if supported (Chrome 42+).
+      chrome.runtime.openOptionsPage();
+    } else {
+      // Reasonable fallback.
+      window.open(chrome.runtime.getURL('pages/options.html'));
+    }
+  });
 
-  
 })(jQuery, chrome, document);
